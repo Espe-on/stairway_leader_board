3d35a514b5130c0c4cdc156304e18478020ee385
espe-on <contact@josephcrawley.dev>
2020-08-26 10:06:36 +0100

    init of loading notifier

---
+++ src/components/loading-notifier/loading-notifier.component.tsx
@@ -0,0 +1,12 @@
+import React, {FunctionComponent} from "react";
+
+import "./loading-notifier.styles.scss"
+
+
+const LoadingNotifier : FunctionComponent = () => (
+    <div className="loading-notifier">
+        Loading . . .
+    </div>
+)
+
+export default LoadingNotifier;
