3c0d8fcd6234362b88d0cad81931f4051e72b01c
espe-on <contact@josephcrawley.dev>
2020-08-26 14:46:05 +0100

    restyling header to match designs

--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,10 +1,12 @@
 import React, {FunctionComponent} from "react";
 
 import "./header.styles.scss"
+import Countdown from "../countdown/countdown.component";
 
-const Header : FunctionComponent = () => (
+const Header: FunctionComponent = () => (
     <div className="header">
         <h1 className="header-title">Compete with your friends this week!</h1>
+        <Countdown/>
     </div>
 );
 
