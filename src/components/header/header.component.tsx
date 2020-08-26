b3483718f8f9da275690776434c6d9734b99ac9e
espe-on <contact@josephcrawley.dev>
2020-08-26 10:05:00 +0100

    init of header component

---
+++ src/components/header/header.component.tsx
@@ -0,0 +1,11 @@
+import React, {FunctionComponent} from "react";
+
+import "./header.styles.scss"
+
+const Header : FunctionComponent = () => (
+    <div className="header">
+        <h1 className="header-title">Compete with your friends this week!</h1>
+    </div>
+);
+
+export default Header;
